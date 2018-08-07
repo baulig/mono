b981e5d568cc6078fb39ab1cdad3002f51047b86
Martin Baulig <mabaul@microsoft.com>
2018-08-06 21:57:45 -0400

    X

--- sdks/mac/xunit/System/launch.sh
+++ sdks/mac/xunit/System/launch.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/bin/mlaunch -v --device=:v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-4,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-X --install-progress --installsim=./bin/iPhoneSimulator/Debug/PhoneTest.app
-/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/bin/mlaunch -v -v -v --device=:v2:runtime=com.apple.CoreSimulator.SimRuntime.iOS-11-4,devicetype=com.apple.CoreSimulator.SimDeviceType.iPhone-X --launchsim=./bin/iPhoneSimulator/Debug/PhoneTest.app --stdout=simulator.log --stderr=simulator.log --wait-for-exit
+./bin/Debug/MacTest.app/Contents/MacOS/MacTest
