0f24b67da94ce24066deeed831159666f5d2f155
Martin Baulig <mabaul@microsoft.com>
2019-08-19 13:36:17 -0400

    Y

---
+++ mcs/class/System/martin/SocketAsyncContext.Mono.cs
@@ -0,0 +1,165 @@
+using Microsoft.Win32.SafeHandles;
+
+using System.Diagnostics;
+using System.Threading;
+
+namespace System.Net.Sockets
+{
+    sealed partial class SocketAsyncContext
+    {
+        readonly SafeSocketHandle _socket;
+
+        public SocketAsyncContext(SafeCloseSocket socket)
+        {
+            // CoreFX uses `SafeCloseSocket`, we are using `SafeSocketHandle`.
+            _socket = (SafeSocketHandle)socket;
+        }
+
+        public void SetNonBlocking()
+        {
+            Socket.Blocking_internal(_socket, false, out var error);
+            if (error != 0)
+                throw new SocketException(error);
+        }
+
+        public SocketError Connect(byte[] socketAddress, int socketAddressLen)
+        {
+            Debug.Assert(socketAddress != null, "Expected non-null socketAddress");
+            Debug.Assert(socketAddressLen > 0, $"Unexpected socketAddressLen: {socketAddressLen}");
+
+            // Connect is different than the usual "readiness" pattern of other operations.
+            // We need to call TryStartConnect to initiate the connect with the OS, 
+            // before we try to complete it via epoll notification. 
+            // Thus, always call TryStartConnect regardless of readiness.
+            SocketError errorCode;
+            int observedSequenceNumber;
+//            _sendQueue.IsReady(this, out observedSequenceNumber);
+            if (SocketPal.TryStartConnect(_socket, socketAddress, socketAddressLen, out errorCode))
+            {
+                _socket.RegisterConnectResult(errorCode);
+                return errorCode;
+            }
+
+            var operation = new ConnectOperation(this)
+            {
+                SocketAddress = socketAddress,
+                SocketAddressLen = socketAddressLen
+            };
+
+//            PerformSyncOperation(ref _sendQueue, operation, -1, observedSequenceNumber);
+
+            return operation.ErrorCode;
+        }
+
+        public SocketError ConnectAsync(byte[] socketAddress, int socketAddressLen, Action<SocketError> callback)
+        {
+            Debug.Assert(socketAddress != null, "Expected non-null socketAddress");
+            Debug.Assert(socketAddressLen > 0, $"Unexpected socketAddressLen: {socketAddressLen}");
+            Debug.Assert(callback != null, "Expected non-null callback");
+
+            SetNonBlocking();
+
+            // Connect is different than the usual "readiness" pattern of other operations.
+            // We need to initiate the connect before we try to complete it. 
+            // Thus, always call TryStartConnect regardless of readiness.
+            SocketError errorCode;
+            if (SocketPal.TryStartConnect(_socket, socketAddress, socketAddressLen, out errorCode))
+            {
+                _socket.RegisterConnectResult(errorCode);
+                return errorCode;
+            }
+
+            var operation = new ConnectOperation(this)
+            {
+                Callback = callback,
+                SocketAddress = socketAddress,
+                SocketAddressLen = socketAddressLen
+            };
+
+            IOSelector.Add (_socket.DangerousGetHandle (), new IOSelectorJob (IOOperation.Write, AsyncOperation.CompletionCallback, operation));
+            return SocketError.IOPending;
+        }
+
+        abstract partial class AsyncOperation : IOAsyncResult
+        {
+            internal sealed override void CompleteDisposed()
+            {
+                Abort();
+            }
+
+            internal static void CompletionCallback(IOAsyncResult ioares)
+            {
+                var operation = (AsyncOperation)ioares;
+                if (operation.TryComplete(operation.AssociatedContext))
+                {
+                    operation.InvokeCallback(true);
+                }
+            }
+        }
+
+#if FIXME
+        abstract class AsyncOperation : IOAsyncResult
+        {
+            public readonly SocketAsyncContext AssociatedContext;
+            protected object CallbackOrEvent;
+            public SocketError ErrorCode;
+            public byte[] SocketAddress;
+            public int SocketAddressLen;
+
+            internal override void CompleteDisposed()
+            {
+                Abort();
+            }
+
+            protected AsyncOperation(SocketAsyncContext context)
+            {
+                AssociatedContext = context;
+            }
+
+            internal static void CompletionCallback(IOAsyncResult ioares)
+            {
+                var operation = (AsyncOperation)ioares;
+                if (operation.TryComplete(operation.AssociatedContext))
+                {
+                    operation.InvokeCallback(true);
+                }
+            }
+
+            public bool TryComplete(SocketAsyncContext context)
+            {
+                return DoTryComplete(context);
+            }
+
+            protected abstract void Abort();
+
+            protected abstract bool DoTryComplete(SocketAsyncContext context);
+
+            public abstract void InvokeCallback(bool allowPooling);
+        }
+
+        class ConnectOperation : AsyncOperation
+        {
+            public Action<SocketError> Callback
+            {
+                set => CallbackOrEvent = value;
+            }
+
+            public ConnectOperation(SocketAsyncContext context) : base(context)
+            {
+            }
+
+            protected override void Abort() { }
+
+            protected override bool DoTryComplete(SocketAsyncContext context)
+            {
+                bool result = SocketPal.TryCompleteConnect(context._socket, SocketAddressLen, out ErrorCode);
+                context._socket.RegisterConnectResult(ErrorCode);
+                return result;
+            }
+
+            public override void InvokeCallback(bool allowPooling) =>
+                ((Action<SocketError>)CallbackOrEvent)(ErrorCode);
+        }
+#endif
+    }
+}
--- mcs/class/System/martin/SocketAsyncContext.cs
+++ mcs/class/System/martin/SocketAsyncContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.Win32.SafeHandles;
-
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace System.Net.Sockets
@@ -8,5 +10,280 @@ namespace System.Net.Sockets
     sealed partial class SocketAsyncContext
     {
 
+        #region CoreFX Code
+
+        private abstract partial class AsyncOperation
+        {
+            private enum State
+            {
+                Waiting = 0,
+                Running = 1,
+                Complete = 2,
+                Cancelled = 3
+            }
+
+            private int _state; // Actually AsyncOperation.State.
+
+#if DEBUG
+            private int _callbackQueued; // When non-zero, the callback has been queued.
+#endif
+
+            public readonly SocketAsyncContext AssociatedContext;
+            public AsyncOperation Next;
+            protected object CallbackOrEvent;
+            public SocketError ErrorCode;
+            public byte[] SocketAddress;
+            public int SocketAddressLen;
+
+            public ManualResetEventSlim Event
+            {
+                get { return CallbackOrEvent as ManualResetEventSlim; }
+                set { CallbackOrEvent = value; }
+            }
+
+            public AsyncOperation(SocketAsyncContext context)
+            {
+                AssociatedContext = context;
+                Reset();
+            }
+
+            public void Reset()
+            {
+                _state = (int)State.Waiting;
+                Next = this;
+#if DEBUG
+                _callbackQueued = 0;
+#endif
+            }
+
+            public bool TryComplete(SocketAsyncContext context)
+            {
+                TraceWithContext(context, "Enter");
+
+                bool result = DoTryComplete(context);
+
+                TraceWithContext(context, $"Exit, result={result}");
+
+                return result;
+            }
+
+            public bool TrySetRunning()
+            {
+                State oldState = (State)Interlocked.CompareExchange(ref _state, (int)State.Running, (int)State.Waiting);
+                if (oldState == State.Cancelled)
+                {
+                    // This operation has already been cancelled, and had its completion processed.
+                    // Simply return false to indicate no further processing is needed.
+                    return false;
+                }
+
+                Debug.Assert(oldState == (int)State.Waiting);
+                return true;
+            }
+
+            public void SetComplete()
+            {
+                Debug.Assert(Volatile.Read(ref _state) == (int)State.Running);
+
+                Volatile.Write(ref _state, (int)State.Complete);
+            }
+
+            public void SetWaiting()
+            {
+                Debug.Assert(Volatile.Read(ref _state) == (int)State.Running);
+
+                Volatile.Write(ref _state, (int)State.Waiting);
+            }
+
+            public bool TryCancel()
+            {
+                Trace("Enter");
+
+                // Try to transition from Waiting to Cancelled
+                var spinWait = new SpinWait();
+                bool keepWaiting = true;
+                while (keepWaiting)
+                {
+                    int state = Interlocked.CompareExchange(ref _state, (int)State.Cancelled, (int)State.Waiting);
+                    switch ((State)state)
+                    {
+                        case State.Running:
+                            // A completion attempt is in progress. Keep busy-waiting.
+                            Trace("Busy wait");
+                            spinWait.SpinOnce();
+                            break;
+
+                        case State.Complete:
+                            // A completion attempt succeeded. Consider this operation as having completed within the timeout.
+                            Trace("Exit, previously completed");
+                            return false;
+
+                        case State.Waiting:
+                            // This operation was successfully cancelled.
+                            // Break out of the loop to handle the cancellation
+                            keepWaiting = false;
+                            break;
+
+                        case State.Cancelled:
+                            // Someone else cancelled the operation.
+                            // Just return true to indicate the operation was cancelled.
+                            // The previous canceller will have fired the completion, etc.
+                            Trace("Exit, previously cancelled");
+                            return true;
+                    }
+                }
+
+                Trace("Cancelled, processing completion");
+
+                // The operation successfully cancelled.  
+                // It's our responsibility to set the error code and queue the completion.
+                DoAbort();
+
+                var @event = CallbackOrEvent as ManualResetEventSlim;
+                if (@event != null)
+                {
+                    @event.Set();
+                }
+                else
+                {
+#if DEBUG
+                    Debug.Assert(Interlocked.CompareExchange(ref _callbackQueued, 1, 0) == 0, $"Unexpected _callbackQueued: {_callbackQueued}");
+#endif
+                    // We've marked the operation as canceled, and so should invoke the callback, but
+                    // we can't pool the object, as ProcessQueue may still have a reference to it, due to
+                    // using a pattern whereby it takes the lock to grab an item, but then releases the lock
+                    // to do further processing on the item that's still in the list.
+                    ThreadPool.UnsafeQueueUserWorkItem(o => ((AsyncOperation)o).InvokeCallback(allowPooling: false), this);
+                }
+
+                Trace("Exit");
+
+                // Note, we leave the operation in the OperationQueue.
+                // When we get around to processing it, we'll see it's cancelled and skip it.
+                return true;
+            }
+
+            public void Dispatch(WaitCallback processingCallback)
+            {
+                ManualResetEventSlim e = Event;
+                if (e != null)
+                {
+                    // Sync operation.  Signal waiting thread to continue processing.
+                    e.Set();
+                }
+                else
+                {
+                    // Async operation.  Process the IO on the threadpool.
+                    ThreadPool.UnsafeQueueUserWorkItem(processingCallback, this);
+                }
+            }
+
+            // Called when op is not in the queue yet, so can't be otherwise executing
+            public void DoAbort()
+            {
+                Abort();
+                ErrorCode = SocketError.OperationAborted;
+            }
+
+            protected abstract void Abort();
+
+            protected abstract bool DoTryComplete(SocketAsyncContext context);
+
+            public abstract void InvokeCallback(bool allowPooling);
+
+            [Conditional("SOCKETASYNCCONTEXT_TRACE")]
+            public void Trace(string message, [CallerMemberName] string memberName = null)
+            {
+                OutputTrace($"{IdOf(this)}.{memberName}: {message}");
+            }
+
+            [Conditional("SOCKETASYNCCONTEXT_TRACE")]
+            public void TraceWithContext(SocketAsyncContext context, string message, [CallerMemberName] string memberName = null)
+            {
+                OutputTrace($"{IdOf(context)}, {IdOf(this)}.{memberName}: {message}");
+            }
+        }
+
+        // These two abstract classes differentiate the operations that go in the
+        // read queue vs the ones that go in the write queue.
+        private abstract class ReadOperation : AsyncOperation 
+        {
+            public ReadOperation(SocketAsyncContext context) : base(context) { }
+        }
+
+        private abstract class WriteOperation : AsyncOperation 
+        {
+            public WriteOperation(SocketAsyncContext context) : base(context) { }
+        }
+
+        private abstract class SendOperation : WriteOperation
+        {
+            public SocketFlags Flags;
+            public int BytesTransferred;
+            public int Offset;
+            public int Count;
+
+            public SendOperation(SocketAsyncContext context) : base(context) { }
+
+            protected sealed override void Abort() { }
+
+            public Action<int, byte[], int, SocketFlags, SocketError> Callback
+            {
+                set => CallbackOrEvent = value;
+            }
+
+            public override void InvokeCallback(bool allowPooling) =>
+                ((Action<int, byte[], int, SocketFlags, SocketError>)CallbackOrEvent)(BytesTransferred, SocketAddress, SocketAddressLen, SocketFlags.None, ErrorCode);
+        }
+
+        private sealed class ConnectOperation : WriteOperation
+        {
+            public ConnectOperation(SocketAsyncContext context) : base(context) { }
+
+            public Action<SocketError> Callback
+            {
+                set => CallbackOrEvent = value;
+            }
+
+            protected override void Abort() { }
+
+            protected override bool DoTryComplete(SocketAsyncContext context)
+            {
+                bool result = SocketPal.TryCompleteConnect(context._socket, SocketAddressLen, out ErrorCode);
+                context._socket.RegisterConnectResult(ErrorCode);
+                return result;
+            }
+
+            public override void InvokeCallback(bool allowPooling) =>
+                ((Action<SocketError>)CallbackOrEvent)(ErrorCode);
+        }
+
+
+        //
+        // Tracing stuff
+        //
+
+        // To enabled tracing:
+        // (1) Add reference to System.Console in the csproj
+        // (2) #define SOCKETASYNCCONTEXT_TRACE
+
+        [Conditional("SOCKETASYNCCONTEXT_TRACE")]
+        public void Trace(string message, [CallerMemberName] string memberName = null)
+        {
+            OutputTrace($"{IdOf(this)}.{memberName}: {message}");
+        }
+
+        [Conditional("SOCKETASYNCCONTEXT_TRACE")]
+        public static void OutputTrace(string s)
+        {
+            // CONSIDER: Change to NetEventSource
+#if SOCKETASYNCCONTEXT_TRACE
+            Console.WriteLine(s);
+#endif
+        }
+
+        public static string IdOf(object o) => o == null ? "(null)" : $"{o.GetType().Name}#{o.GetHashCode():X2}";
+
+#endregion
     }
 }
